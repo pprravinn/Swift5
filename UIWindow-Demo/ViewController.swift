149f8270d1605151a9face8fe3d6c59c1d5424c0
Pravin Kamble <pprravinn@gmail.com>
2020-03-08 22:43:14 +0530

    Initial Commit

---
+++ UIWindow-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIWindow-Demo
+//
+//  Created by Pravin Kamble on 08/03/20.
+//  Copyright © 2020 Pravin Kamble. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
